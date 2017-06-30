1d0363b1e3c4e9f40ffd8c74b5e7a13b100b1f3c
Brian Bolnick <bbolnick@instructure.com>
2017-06-29 22:49:33 -0600

    remove dj

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,5 @@ module Bungie
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.active_job.queue_adapter = :delayed_job
   end
 end
