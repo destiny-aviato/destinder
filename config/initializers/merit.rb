e128134b068df03187ba7411590534c888c772f6
Brian Bolnick <bbolnick@instructure.com>
2017-08-21 15:23:58 -0600

    Update badges

--- config/initializers/merit.rb
+++ config/initializers/merit.rb
@@ -37,7 +37,7 @@ Merit::Badge.create!(
   name: "Donator",
   description: "Donated at least $5!",
   custom_fields: { 
-    icon: '<i class="material-icons" style="float: left; font-size: 14px; line-height: 23px; padding-right: 3px; margin-left: -8px;">attach_money</i>',
+    icon: '<i class="material-icons" style="float: left; font-size: 14px; line-height: 23px; padding-right: 4px; margin-left: -6px;">attach_money</i>',
     color: "color: #FAFAFA; background-color: #51de90;"
   }
 )
@@ -47,7 +47,7 @@ Merit::Badge.create!(
   name: "Architect",
   description: "Site Developer",
   custom_fields: { 
-    icon: '<i class="material-icons" style="float: left; font-size: 14px; line-height: 23px; padding-right: 3px; margin-left: -8px;">attach_money</i>',
+    icon: '<i class="material-icons" style="float: left; font-size: 14px; line-height: 23px; padding-right: 3px; margin-left: -8px;">developer_mode</i>',
     color: "color: #FAFAFA; background-color: #2f3337;"
    }
 )
@@ -56,7 +56,7 @@ Merit::Badge.create!(
   name: "Donator",
   description: "Donated at least $50!",
   custom_fields: { 
-    icon: '<i class="material-icons" style="float: left; font-size: 14px; line-height: 23px; padding-right: 3px; margin-left: -8px;">attach_money</i>',
+    icon: '<i class="material-icons" style="float: left; font-size: 14px; line-height: 23px; padding-right: 4px; margin-left: -6px;">attach_money</i>',
     color: "color: #FAFAFA; background-color: #B4B8BC;"
    }
 )
@@ -66,7 +66,7 @@ Merit::Badge.create!(
   name: "Sponsor",
   description: "Donated at least $100!",
   custom_fields: { 
-    icon: '<i class="material-icons" style="float: left; font-size: 14px; line-height: 23px; padding-right: 3px; margin-left: -8px;">attach_money</i>',
+    icon: '<i class="material-icons" style="float: left; font-size: 14px; line-height: 23px; padding-right: 4px; margin-left: -6px;">attach_money</i>',
     color: "color: #FAFAFA; background-color: #FFCC01;"
    }
 )
@@ -76,7 +76,7 @@ Merit::Badge.create!(
   name: "Veteran",
   description: "One of the first 250 users on the site!",
   custom_fields: { 
-    icon: '<i class="material-icons" style="float: left; font-size: 14px; line-height: 23px; padding-right: 3px; margin-left: -8px;">hot_tub</i>',
+    icon: '<i class="fa fa-first-order" style="float: left; font-size: 14px; line-height: 23px; padding-right: 4px; margin-left: -6px;"></i>',
     color: "color: #FAFAFA; background-color: #026670;"
    }
 )
