a922ef3408eeaa8a24b6298bcb9c6fdb308e1f93
Brian Bolnick <bbolnick@instructure.com>
2017-08-23 20:44:22 -0600

    update post form and options

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -18,6 +18,7 @@ class MicropostsController < ApplicationController
     def create
 
         @micropost = current_user.microposts.build(micropost_params)
+        @micropost.raid_difficulty = "Normal" if @micropost.raid_difficulty.nil? 
         case @micropost.game_type
         when "Trials of Osiris" 
             @micropost.raid_difficulty = ""
