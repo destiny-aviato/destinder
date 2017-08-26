c05d2defcfb63f564409731309c45843d4ac5515
alexwalz <alexwalz@icloud.com>
2017-08-26 01:19:49 -0600

    stat updates to player profile page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -60,7 +60,7 @@ $(document).on('turbolinks:load', function() {
     $('#checkpoint').hide();
     $('div#difficulty').hide();
     $('label.difficulty-label').hide();
-    
+
     $('#micropost_raid_difficulty').val('Normal');
 
     $("#micropost_raid_difficulty").click(function() {
@@ -121,15 +121,15 @@ $(document).on('turbolinks:load', function() {
 
 
     $('.parallax').parallax();
-    $('.tooltipped').tooltip({ 
+    $('.tooltipped').tooltip({
         delay: 50,
         html: true
-     });
+    });
 
 
     $('#pve-game-select').change(function() {
         selection = $(this).val();
-        
+
 
         if ($.inArray(selection, ["Wrath of the Machine", "King's Fall", "Crota's End", "Vault of Glass"]) >= 0) {
             $('#pve-gametype-select').attr('class', 'input-field col s12 m6');
@@ -160,10 +160,10 @@ $(document).on('click', '.notification > button.delete', function() {
 $(document).on("turbolinks:load", function() {
     $('.modal').modal({
         dismissible: true,
-        opacity: .5, 
-        inDuration: 300, 
-        outDuration: 200, 
-        startingTop: '4%', 
+        opacity: .5,
+        inDuration: 300,
+        outDuration: 200,
+        startingTop: '4%',
         endingTop: '10%'
     });
 
@@ -196,7 +196,7 @@ $(document).on("turbolinks:load", function() {
     $('.slider').slider('prev');
 
     $("h1#home-header").hide().fadeIn(4000);
-    $('.tooltip').tooltipster({ 
+    $('.tooltip').tooltipster({
         theme: ['tooltipster-noir', 'tooltipster-noir-customized'],
         delay: 50
     });
@@ -207,128 +207,126 @@ $(document).on("turbolinks:load", function() {
         // var char_data = chart_data[0];
         var weaponctx1 = document.getElementById("weapon-breakdown-chart1").getContext('2d');
         var weaponChart1 = new Chart(weaponctx1, {
-          type: 'polarArea',
-          data: {
-            labels: ["Auto Rifle", "Hand Cannon", "Pulse Rifle", "Scout Rifle", "Sniper", "Shotgun"],
-            datasets: [{
-              backgroundColor: [
-                "#2ecc71",
-                "#3498db",
-                "#95a5a6",
-                "#9b59b6",
-                "#f1c40f",
-                "#e74c3c",
-                "#34495e"
-              ],
-              data: [char_data1["Character Stats"]["Kill Stats"]["Auto Rifle"], char_data1["Character Stats"]["Kill Stats"]["Hand Cannon"], char_data1["Character Stats"]["Kill Stats"]["Pulse Rifle"], char_data1["Character Stats"]["Kill Stats"]["Scout Rifle"], char_data1["Character Stats"]["Kill Stats"]["Sniper"], char_data1["Character Stats"]["Kill Stats"]["Shotgun"]]
-            }]
-          }
+            type: 'polarArea',
+            data: {
+                labels: ["Auto Rifle", "Hand Cannon", "Pulse Rifle", "Scout Rifle", "Sniper", "Shotgun"],
+                datasets: [{
+                    backgroundColor: [
+                        "#2ecc71",
+                        "#3498db",
+                        "#95a5a6",
+                        "#9b59b6",
+                        "#f1c40f",
+                        "#e74c3c",
+                        "#34495e"
+                    ],
+                    data: [char_data1["Character Stats"]["Kill Stats"]["Auto Rifle"], char_data1["Character Stats"]["Kill Stats"]["Hand Cannon"], char_data1["Character Stats"]["Kill Stats"]["Pulse Rifle"], char_data1["Character Stats"]["Kill Stats"]["Scout Rifle"], char_data1["Character Stats"]["Kill Stats"]["Sniper"], char_data1["Character Stats"]["Kill Stats"]["Shotgun"]]
+                }]
+            }
         });
-    
+
         var char_data2 = JSON.parse($(".chart-container2").attr("data-chart-data"));
         // var char_data = chart_data[0];
         var weaponctx2 = document.getElementById("weapon-breakdown-chart2").getContext('2d');
         var weaponChart2 = new Chart(weaponctx2, {
-          type: 'polarArea',
-          data: {
-            labels: ["Auto Rifle", "Hand Cannon", "Pulse Rifle", "Scout Rifle", "Sniper", "Shotgun"],
-            datasets: [{
-              backgroundColor: [
-                "#2ecc71",
-                "#3498db",
-                "#95a5a6",
-                "#9b59b6",
-                "#f1c40f",
-                "#e74c3c",
-                "#34495e"
-              ],
-              data: [char_data2["Character Stats"]["Kill Stats"]["Auto Rifle"], char_data2["Character Stats"]["Kill Stats"]["Hand Cannon"], char_data2["Character Stats"]["Kill Stats"]["Pulse Rifle"], char_data2["Character Stats"]["Kill Stats"]["Scout Rifle"], char_data2["Character Stats"]["Kill Stats"]["Sniper"], char_data2["Character Stats"]["Kill Stats"]["Shotgun"]]
-            }]
-          }
+            type: 'polarArea',
+            data: {
+                labels: ["Auto Rifle", "Hand Cannon", "Pulse Rifle", "Scout Rifle", "Sniper", "Shotgun"],
+                datasets: [{
+                    backgroundColor: [
+                        "#2ecc71",
+                        "#3498db",
+                        "#95a5a6",
+                        "#9b59b6",
+                        "#f1c40f",
+                        "#e74c3c",
+                        "#34495e"
+                    ],
+                    data: [char_data2["Character Stats"]["Kill Stats"]["Auto Rifle"], char_data2["Character Stats"]["Kill Stats"]["Hand Cannon"], char_data2["Character Stats"]["Kill Stats"]["Pulse Rifle"], char_data2["Character Stats"]["Kill Stats"]["Scout Rifle"], char_data2["Character Stats"]["Kill Stats"]["Sniper"], char_data2["Character Stats"]["Kill Stats"]["Shotgun"]]
+                }]
+            }
         });
-    
+
         var char_data3 = JSON.parse($(".chart-container3").attr("data-chart-data"));
         var weaponctx3 = document.getElementById("weapon-breakdown-chart3").getContext('2d');
         var weaponChart3 = new Chart(weaponctx3, {
-          type: 'polarArea',
-          options: {
-            layout: {
-                padding: {
-                    top: 20
+            type: 'polarArea',
+            options: {
+                layout: {
+                    padding: {
+                        top: 20
                     }
                 }
             },
-          data: {
-            labels: ["Auto Rifle", "Hand Cannon", "Pulse Rifle", "Scout Rifle", "Sniper", "Shotgun"],
-            datasets: [{
-              backgroundColor: [
-                "#2ecc71",
-                "#3498db",
-                "#95a5a6",
-                "#9b59b6",
-                "#f1c40f",
-                "#e74c3c",
-                "#34495e"
-              ],
-              data: [char_data3["Character Stats"]["Kill Stats"]["Auto Rifle"], char_data3["Character Stats"]["Kill Stats"]["Hand Cannon"], char_data3["Character Stats"]["Kill Stats"]["Pulse Rifle"], char_data3["Character Stats"]["Kill Stats"]["Scout Rifle"], char_data3["Character Stats"]["Kill Stats"]["Sniper"], char_data3["Character Stats"]["Kill Stats"]["Shotgun"]]
-            }]
-          }
+            data: {
+                labels: ["Auto Rifle", "Hand Cannon", "Pulse Rifle", "Scout Rifle", "Sniper", "Shotgun"],
+                datasets: [{
+                    backgroundColor: [
+                        "#2ecc71",
+                        "#3498db",
+                        "#95a5a6",
+                        "#9b59b6",
+                        "#f1c40f",
+                        "#e74c3c",
+                        "#34495e"
+                    ],
+                    data: [char_data3["Character Stats"]["Kill Stats"]["Auto Rifle"], char_data3["Character Stats"]["Kill Stats"]["Hand Cannon"], char_data3["Character Stats"]["Kill Stats"]["Pulse Rifle"], char_data3["Character Stats"]["Kill Stats"]["Scout Rifle"], char_data3["Character Stats"]["Kill Stats"]["Sniper"], char_data3["Character Stats"]["Kill Stats"]["Shotgun"]]
+                }]
+            }
         });
-    
+
         //////// ABILITY CHARTS ////////////
         // var char_data1 = JSON.parse($(".chart-container1").attr("data-chart-data"));
         var abilityctx1 = document.getElementById("ability-chart-breakdown1").getContext('2d');
         var abilityChart1 = new Chart(abilityctx1, {
-          type: 'doughnut',
-          data: {
-            labels: ["Intellect", "Discipline", "Strength"],
-            datasets: [{
-              backgroundColor: [
-                "#2ecc71",
-                "#3498db",
-                "#34495e"
-              ],
-              data: [char_data1["Character Stats"]["Intellect"], char_data1["Character Stats"]["Discipline"], char_data1["Character Stats"]["Strength"]]
-            }]
-          }
+            type: 'doughnut',
+            data: {
+                labels: ["Intellect", "Discipline", "Strength"],
+                datasets: [{
+                    backgroundColor: [
+                        "#2ecc71",
+                        "#3498db",
+                        "#34495e"
+                    ],
+                    data: [char_data1["Character Stats"]["Intellect"], char_data1["Character Stats"]["Discipline"], char_data1["Character Stats"]["Strength"]]
+                }]
+            }
         });
-    
+
         // var char_data1 = JSON.parse($(".chart-container1").attr("data-chart-data"));
         var abilityctx2 = document.getElementById("ability-chart-breakdown2").getContext('2d');
         var abilityChart2 = new Chart(abilityctx2, {
-          type: 'doughnut',
-          data: {
-            labels: ["Intellect", "Discipline", "Strength"],
-            datasets: [{
-              backgroundColor: [
-                "#2ecc71",
-                "#3498db",
-                "#34495e"
-              ],
-              data: [char_data2["Character Stats"]["Intellect"], char_data2["Character Stats"]["Discipline"], char_data2["Character Stats"]["Strength"]]
-            }]
-          }
+            type: 'doughnut',
+            data: {
+                labels: ["Intellect", "Discipline", "Strength"],
+                datasets: [{
+                    backgroundColor: [
+                        "#2ecc71",
+                        "#3498db",
+                        "#34495e"
+                    ],
+                    data: [char_data2["Character Stats"]["Intellect"], char_data2["Character Stats"]["Discipline"], char_data2["Character Stats"]["Strength"]]
+                }]
+            }
         });
-    
+
         // var char_data1 = JSON.parse($(".chart-container1").attr("data-chart-data"));
         var abilityctx3 = document.getElementById("ability-chart-breakdown3").getContext('2d');
         var abilityChart3 = new Chart(abilityctx3, {
-          type: 'doughnut',
-          data: {
-            labels: ["Intellect", "Discipline", "Strength"],
-            datasets: [{
-              backgroundColor: [
-                "#2ecc71",
-                "#3498db",
-                "#34495e"
-              ],
-              data: [char_data3["Character Stats"]["Intellect"], char_data3["Character Stats"]["Discipline"], char_data3["Character Stats"]["Strength"]]
-            }]
-          }
+            type: 'doughnut',
+            data: {
+                labels: ["Intellect", "Discipline", "Strength"],
+                datasets: [{
+                    backgroundColor: [
+                        "#2ecc71",
+                        "#3498db",
+                        "#34495e"
+                    ],
+                    data: [char_data3["Character Stats"]["Intellect"], char_data3["Character Stats"]["Discipline"], char_data3["Character Stats"]["Strength"]]
+                }]
+            }
         });
-     }
-    
-
+    }
 
 
 
