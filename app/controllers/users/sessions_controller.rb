a9629f82893f0f466d16a66922886dd1c8d9e370
Brian Bolnick <bbolnick@instructure.com>
2017-08-31 09:02:30 -0600

    add badges automatically, and some UI bug fixes

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -7,9 +7,10 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    super
+    puts "test"
+  end
 
   # DELETE /resource/sign_out
   # def destroy
