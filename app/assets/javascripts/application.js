dfdab8aefaa0977da6822ff89e33dd749d88ef9d
alexwalz <alexwalz@icloud.com>
2017-09-14 15:55:38 -0600

    updates to the homepage

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1006,3 +1006,35 @@ $(document).on("turbolinks:load", function() {
         $('#load-link').click();
     }
 });
+
+
+var options = [{
+        selector: '#staggered-test',
+        offset: 50,
+        callback: function(el) {
+            Materialize.toast("This is our ScrollFire Demo!", 1500);
+        }
+    },
+    {
+        selector: '#staggered-test',
+        offset: 205,
+        callback: function(el) {
+            Materialize.toast("Please continue scrolling!", 1500);
+        }
+    },
+    {
+        selector: '#staggered-test',
+        offset: 400,
+        callback: function(el) {
+            Materialize.showStaggeredList($(el));
+        }
+    },
+    {
+        selector: '#image-test',
+        offset: 500,
+        callback: function(el) {
+            Materialize.fadeInImage($(el));
+        }
+    }
+];
+Materialize.scrollFire(options);
