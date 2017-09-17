43de584a893f593390ba31ba6eff44eb97ff1292
Brian Bolnick <bbolnick@instructure.com>
2017-09-17 09:53:44 -0600

    starts for home page updates and auth changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :record_user_activity, :authenticate_user!, :except => [:application_error]
-
+  before_action :record_user_activity
     def after_sign_in_path_for(resource)
       request.env['omniauth.origin'] || root_path
    end
